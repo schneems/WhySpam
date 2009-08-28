5ba3d618b29186784371d1fdcdbfe9a8c52f0f13
Richard Schneeman <thedickster@gmail.com>
2009-08-28 10:35:02 -0500

    cleanup

--- public/javascripts/WhySpamExtern.js
+++
@@ -1,3 +0,0 @@
-website = 'http://localhost:3000/forms/foreign_show/'+form_id;
-document.write("<iframe scrolling=auto src='"+website+"' frameborder=0 allowtransparency=true style='width:100%;height:100%'></iframe>");
-
