b37684ffcb880788b1b3fc27179339ed93ef3c82
Richard Schneeman <thedickster@gmail.com>
2009-09-19 19:49:45 -0500

    routes again

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :spamfire
   
   map.widget '/widget/show/:url', :controller => "grade_widget", :action => "show"
-  map.widget_create '/widget/create/:website', :controller => "grade_widget", :action => "create"
+  map.widget_create '/widget/create', :controller => "grade_widget", :action => "create"
   
   
   map.show_website 'website/:url', :controller => 'websites', :action => 'show'
