d4842b2d77c9dad59b901aeedd6b7a110699c684
Richard Schneeman <thedickster@gmail.com>
2009-10-25 16:26:44 -0500

    removed packed gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,4 +55,11 @@ end
 
 
 
-after 'deploy:update_code', 'deploy:symlink_shared'
+after 'deploy:update_code', 'deploy:symlink_shared', "deploy:symlink", "deploy:update_crontab"
+
+namespace :deploy do
+  desc "Update the crontab file"
+  task :update_crontab, :roles => :db do
+    run "cd #{release_path} && whenever --update-crontab #{application}"
+  end
+end
--- config/schedule.rb
+++ config/schedule.rb
@@ -10,9 +10,3 @@ end
 
 after "deploy:symlink", "deploy:update_crontab"
 
-namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application}"
-  end
-end
