47988dd6d0267130342285f73eb731a5e35ad099
RichardSchneeman <richardschneeman@mp3-wireless-10-0-32-148.amer.corp.natinst.com>
2009-11-11 14:02:24 -0600

    changed position of demo changed min-width of secure forms

--- public/javascripts/WhySpamExtern1a72jd6x1z.js
+++ public/javascripts/WhySpamExtern1a72jd6x1z.js
@@ -1,3 +1,3 @@
 website = 'http://whyspam.me/forms/foreign_show/'+form_id;
-document.write("<iframe scrolling=auto src='"+website+"' frameborder=0 allowtransparency=true style='width:475px; min-height:450px'></iframe>");
+document.write("<iframe scrolling=auto src='"+website+"' frameborder=0 allowtransparency=true style='width:475px; min-height:500px'></iframe>");
 
