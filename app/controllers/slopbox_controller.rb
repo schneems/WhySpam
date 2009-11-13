4462ca18690d911d4aedd7ccc9a2b8246450d73d
RichardSchneeman <richardschneeman@richard-schneemans-macbook-pro.local>
2009-11-13 10:40:13 -0600

    fixed production errors from googlebot

--- app/controllers/slopbox_controller.rb
+++ app/controllers/slopbox_controller.rb
@@ -5,10 +5,11 @@ class SlopboxController < ApplicationController
    def show
         flash[:error] = ""
         #  @ticket = Ticket.first
-        @to_email = params[:ticket][:to_email]  
         
         #@ticket = Ticket.new(:to_email => @to_email)       
           if simple_captcha_valid?
+            @to_email = params[:ticket][:to_email] 
+            
             email_length = @to_email.scan(/[\w.]+@/)[0].to_s.size           
             @tickets = Ticket.find(:all, :conditions => ["to_email = ?", @to_email])
             whymail = Whymail.find(:first, :conditions => ["email = ?", @to_email])
@@ -17,15 +18,15 @@ class SlopboxController < ApplicationController
                 flash[:notice] = "You've got mail"
               else 
                 flash[:error]  += "- We haven't received any email at this email address, please try again later. <br />" if @tickets.first.nil?
-                redirect_to :back
+                flash[:error]  += "- This is an address that may be used by a WhySpam Secure Email in the future, <br /> to ensure that you never pick a WhySpam Secure email by accident, always pick a url that does not equal 20 characters such as:  1#{@to_email}<br />" if email_length == 21 
+                redirect_to :action => :index
               end
             else
               flash[:error]  += "- The Text from the Image did not Match the text, please try again <br />"
-              redirect_to :back
+              redirect_to :action => :index
           end
         # flash[:error]  += "- This email is being used by a WhySpam Secure Email.<br />" if whymail != nil
         
-         flash[:error]  += "- This is an address that may be used by a WhySpam Secure Email in the future, <br /> to ensure that you never pick a WhySpam Secure email by accident, always pick a url that does not equal 20 characters such as:  1#{@to_email}<br />" if email_length == 21 
          flash[:error] = nil if flash[:error].strip.empty?
    end
    
