6f1427981a91a1cbe605dc19345e53b1750f644f
RichardSchneeman <richardschneeman@mp3-wireless-10-0-32-183.amer.corp.natinst.com>
2009-12-15 11:12:35 -0600

    new youtube video and change in from address foo_at_example.com@whyspam.me

--- lib/cleanurl.rb
+++ lib/cleanurl.rb
@@ -2,8 +2,10 @@ module Cleanurl
 ##   http://dynamic.boingboing.net/cgi-bin/mt/mt-cp.cgi?__mode=register&blog_id=1&return_to=http%3A%2F%2Fboingboing.net%2F
   def find_from_url(address)
     if !address.nil?
-      address = /\@(.)+/.match(address)[0]
-      address = address[1,address.size]    
+      address = address.gsub(/@/, '_at_') ## removes www
+      
+  #    address = /\@(.)+/.match(address)[0]
+  #    address = address[1,address.size]    
     end
     return address
   end
