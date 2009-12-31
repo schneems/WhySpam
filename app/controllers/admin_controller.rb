a3018eb98ddc040499deb662996577d89939ff47
RichardSchneeman <richardschneeman@richard-schneemans-macbook-pro.local>
2009-12-30 21:36:34 -0600

    admin reporting charts

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,29 +2,40 @@ class AdminController < ApplicationController
   before_filter :require_user
 
   def index
+        time = Time.now - 7.days
+        @user_total_count = User.count
+
+        users = User.find(:all, :conditions => [ "created_at > ?",  (Time.now.beginning_of_day - 1.month) ],:order => 'created_at DESC')
+    #    @users_month = User.find(:all, :conditions => [ "created_at < ?",  Time.now - 1.month ],:order => 'created_at DESC')
+
+
+        @user_per_month = Array.new(30, 0)
+        users.each do |user|
+            0.upto(30) do |date|
+
+              if user.created_at > (Time.now.beginning_of_day - (date+1).days) && user.created_at < (Time.now.beginning_of_day - date.days)
+                @user_per_month[date] =+ 1 
+              end
+            end
+        end
+
+        tickets = Ticket.find(:all, :conditions => [ "created_at > ?",  (Time.now.beginning_of_day - 1.month) ],:order => 'created_at DESC')
+
+        @ticket_per_month = Array.new(30, 0)
+        tickets.each do |ticket|
+            0.upto(30) do |date|
+              if ticket.created_at > (Time.now.beginning_of_day - (date+1).days) && ticket.created_at < (Time.now.beginning_of_day - date.days)
+                @ticket_per_month[date] =+ 1 
+              end
+            end
+        end
+
     
   end
   
   
   def show_users
-    time = Time.now - 7.days
-    @user_total_count = User.count
-    
-    users = User.find(:all, :conditions => [ "created_at > ?",  (Time.now.beginning_of_day - 1.month) ],:order => 'created_at DESC')
-#    @users_month = User.find(:all, :conditions => [ "created_at < ?",  Time.now - 1.month ],:order => 'created_at DESC')
-    
-    
-    @month = Array.new(45, 0)
-    users.each do |user|
-        0.upto(45) do |date|
-          
-          if user.created_at > (Time.now.beginning_of_day - (date+1).days) && user.created_at < (Time.now.beginning_of_day - date.days)
-            @month[date] =+ 1 
-          end
-        end
-    end
 
-    
   end
 
   protected
