52bb2716bc46678121c59f806e283bc72eec9fac
RichardSchneeman <richardschneeman@richard-schneemans-macbook-pro.local>
2010-06-20 10:30:43 -0500

    deleted abingo from model

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,15 +2,6 @@ class Application < ActiveRecord::Base
   include Cleanurl
   include Fwdemail
   
-  before_filter :set_abingo_identity
-  
-  protected
-  def set_abingo_identity
-    if (session[:abingo_identity])
-      Abingo.identity = session[:abingo_identity]
-    else
-      session[:abingo_identity] = Abingo.identity = rand(10 ** 10).to_i
-    end
-  end
+
   
 end
