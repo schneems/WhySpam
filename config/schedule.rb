62e3df8d86c5711efff1153b26c4f2b1bcfd7f99
Schneems <richard.schneeman@gmail.com>
2010-12-23 12:32:17 -0600

    stopping whenever cron tasks on server, uncomment to run

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,16 @@
-every 1.minutes do
-  ## command "/usr/bin/ruby /home/demo/public_html/whyspam.me/current/script/runner /home/demo/public_html/whyspam.me/current/lib/getmail.rb -e production"
-   rake "mailbox:check"
-end
-
-every 30.minutes do
-  ## command "/usr/bin/ruby /home/demo/public_html/whyspam.me/current/script/runner /home/demo/public_html/whyspam.me/current/lib/getmail.rb -e production"
-  rake "mailbox:isworking"
-end
-
-
-every 6.hours do
-  rake "cleanup:old_tickets"
-end
+# uncomment to run tasks with whenever
+#  every 1.minutes do
+#    ## command "/usr/bin/ruby /home/demo/public_html/whyspam.me/current/script/runner /home/demo/public_html/whyspam.me/current/lib/getmail.rb -e production"
+#     rake "mailbox:check"
+#  end
+#  
+#  every 30.minutes do
+#    ## command "/usr/bin/ruby /home/demo/public_html/whyspam.me/current/script/runner /home/demo/public_html/whyspam.me/current/lib/getmail.rb -e production"
+#    rake "mailbox:isworking"
+#  end
+#  
+#  
+#  every 6.hours do
+#    rake "cleanup:old_tickets"
+#  end
 
